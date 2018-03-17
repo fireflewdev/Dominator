c46a941e7ee5eebd045d915eaefe534447dd7227
Fireflew <gordi@tenev.com>
2018-03-17 14:23:20 -0500

    added a few changes

--- src/DisplayEngine.java
+++ src/DisplayEngine.java
@@ -10,6 +10,8 @@ import javax.swing.*;
 
 
 public class DisplayEngine {
+    private Unit[][] board;
+
     private JFrame frame = new JFrame();
     private JMenuBar menubar;
     private JMenu
@@ -33,23 +35,49 @@ public class DisplayEngine {
             C_PURPLE = Color.decode("#a64ac9"),
             C_PINK = Color.decode("#f172a1");
 
-
-
-
+    //init given nothing
     public DisplayEngine(){
         initFrame();
-        menus();
+        //mFile
+        menubar = new JMenuBar();
+        mFile = new JMenu("File");
+        m_newgame = new JMenuItem("New game");
+        m_loadgame = new JMenuItem("Load game");
+        mFile.add(m_newgame);
+        mFile.add(m_loadgame);
+        menubar.add(mFile);
+        frame.setJMenuBar(menubar);
+
+        JLabel noNewGame = new JLabel("<html>Welcome to Dominator!<br>Press File -> New game to make a new game</html>");
+        noNewGame.setHorizontalAlignment(JLabel.CENTER);
+        frame.add(noNewGame);
+        frame.revalidate();
+
+
     }
+
+    //init given name
+    public DisplayEngine(String name){
+        board = new Unit[500][500];
+        initFrame();
+        menus(name);
+    }
+
+    //init given save file
+    public DisplayEngine(File input){
+
+    }
+
     public void initFrame(){
         frame.setSize(800, 800);
         //frame.getContentPane().add(this);
         frame.setLocationRelativeTo(null);
-        //frame.setBackground(BG);
+        frame.getContentPane().setBackground(GROUND);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(true);
         frame.setVisible(true);
     }
-    public void menus(){
+    public void menus(String name){
         //mFile
         menubar = new JMenuBar();
         mFile = new JMenu("File");
@@ -61,9 +89,9 @@ public class DisplayEngine {
         mFile.add(m_loadgame);
 
         //mGame
-        mCountry = new JMenu("Country");
-        c_prefs = new JMenuItem("Country Settings");
-        c_stats = new JMenuItem("Country Stats");
+        mCountry = new JMenu(name);
+        c_prefs = new JMenuItem("Settings");
+        c_stats = new JMenuItem("Stats");
         mCountry.add(c_prefs);
         mCountry.add(c_stats);
 
@@ -72,4 +100,8 @@ public class DisplayEngine {
         frame.setJMenuBar(menubar);
         frame.revalidate();
     }
+    public void newgame(ActionEvent evt){
+        System.out.print("worked");
+    }
+
 }
---
+++ src/Unit.java
@@ -0,0 +1,4 @@
+//square on the game board. irl gonna be a hexagon or something like that
+
+public class Unit {
+}
